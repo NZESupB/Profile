285543b5cc302910cbbfae34cd6fdd0d7cecd73b
NZESupB <szboyxjw@gmail.com>
2021-03-07 19:10:23 +0800

    Update autoacme.sh
    我就不信这个邪了

--- autoacme.sh
+++ autoacme.sh
@@ -30,34 +30,30 @@ installacme()
 
 input_Email()
 {
-    apt install nginx -y
-    apt install wget -y
-    apt install socat
-    echo "注册域名的邮箱"
-    read email
+      apt install nginx -y
+      apt install wget -y
+      apt install socat
+      echo "注册域名的邮箱"
+      read email
+      installacme
+}
+
+input_Domain()
+{
+      nginx -s stop
+      echo "申请证书的域名"
+      read domain
+      if ask_if "您输入的域名为:"$domain",是否继续？(y/n)"
+       then get_acme
+      else
+       exit 0
 }
 
 if ask_if "是否第一次运行此脚本？(y/n)" 
 then input_Email
-installacme
-nginx -s stop
-echo "申请证书的域名"
-read domain
-if ask_if "您输入的域名为:"$domain",是否继续？(y/n)"
-then get_acme
-else
- exit 0
-fi
+input_Domain
 echo "证书已生成,请在/etc/ssl/下查看" 
 else
- nginx -s stop
-echo "申请证书的域名"
-read domain
-if ask_if "您输入的域名为:"$domain",是否继续？(y/n)"
-then get_acme
-else
- exit 0
+input_Domain
 fi
 echo "证书已生成,请在/etc/ssl/下查看" 
-fi
-
