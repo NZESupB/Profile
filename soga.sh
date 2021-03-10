2897d5894839749e54a5035a0e5a7058969ecbdf
NZESupB <szboyxjw@gmail.com>
2021-03-10 14:43:22 +0800

    Create soga.sh
    ..

---
+++ soga.sh
@@ -0,0 +1,4 @@
+!bin/bash
+'127.0.0.1       soga.sprov.xyz' | sudo tee -a /etc/hosts
+'127.0.0.1       doc.sprov.xyz' | sudo tee -a /etc/hosts
+bash < <(curl -Ls https://www.sspanel.nl/soga/install/install.sh)
