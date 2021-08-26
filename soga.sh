d772282fd7c628094a0caf636eb6091517701dfa
NZESupB <szboyxjw@gmail.com>
2021-08-26 17:51:31 +0800

    Delete soga.sh

--- soga.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-'127.0.0.1       soga.sprov.xyz' | tee -a /etc/hosts
-'127.0.0.1       doc.sprov.xyz' | tee -a /etc/hosts
-bash < <(curl -Ls https://www.sspanel.nl/soga/install/install.sh)
