0bc44cc56f216ecc8760076df9588363f3badb07
NZESupB <szboyxjw@gmail.com>
2022-04-17 15:41:15 +0800

    update domain to nxnow.top

--- snippets/rename.js
+++
@@ -1,7 +0,0 @@
-function rename(node) {
-	const info = JSON.parse(node.ProxyInfo);
-	const hostname = info.Hostname;
-	if (hostname.includes("clashcloud"))
-		return node.Remark + " |薯条";
-	return node.Remark;
-}
